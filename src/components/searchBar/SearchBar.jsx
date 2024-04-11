19895361408cc01187a3d370cd3eb222dbf0f4eb
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-04-11 09:40:16 -0600

    Refactor SearchBar component to use SCSS modules

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import styles from "./SearchBar.module.scss";
+
 const SearchBar = ({ className }) => {
     return (
-        <div className={className}>
-            <input type="text" placeholder="Search..." />
+        <div className={`${className} ${styles.SearchBar}`}>
+            <input
+                className={styles.input}
+                type="text"
+                placeholder="Search..."
+            />
         </div>
     );
 };
