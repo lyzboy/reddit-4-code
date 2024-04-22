efc09365e478492f35b691365aa654270b56c4f6
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-04-22 10:01:05 -0600

    Refactor Post component and update styling

--- src/containers/post/Post.jsx
+++ src/containers/post/Post.jsx
@@ -4,41 +4,53 @@ import styles from "./Post.module.scss";
 
 import testImage from "../../assets/images/pexels-pixabay-259984.jpg";
 
+import { FormatLocalDate } from "../../utils/utils";
+
 const Post = ({ className }) => {
+    const postName =
+        "A coder at work on their spaghetti code. Sometimes these titles can be waaaay longer than expected. If you're not careful, you might end up with a title that's too long for the card. And if I add more and more and more.";
+
+    const postText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+
+    const votes = 100;
+
+    const author = "ZzWingManzZ";
+
+    const commentCount = 55;
+
     return (
         <div className={`${className} ${styles.Post}`}>
-            <h2
-                className={`${styles.Post__name} ${styles.testBox} ${styles.small}`}
-            >
-                Post
+            <h2 className={`${styles.Post__name} ${styles.small}`}>
+                {postName}
             </h2>
-            <div
-                className={`${styles.Post__arrows} ${styles.testBox} ${styles.big}`}
-            >
-                <p>Arrows</p>
+            <div className={`${styles.Post__arrows} ${styles.big}`}>
+                <span
+                    className={`material-symbols-outlined ${styles.Post__arrows__upvote}`}
+                >
+                    arrow_upward
+                </span>
+                <p>{votes}</p>
+                <span
+                    className={`material-symbols-outlined ${styles.Post__arrows__downvote}`}
+                >
+                    arrow_downward
+                </span>
             </div>
-            <div
-                className={`${styles.Post__image} ${styles.testBox} ${styles.big}`}
-            >
+            <div className={`${styles.Post__image} ${styles.big}`}>
                 <div className={styles.imageContainer}>
                     <img src={testImage} />
                 </div>
-                <p>This is the writing for the post</p>
+                <p>{postText}</p>
             </div>
-            <div
-                className={`${styles.Post__date} ${styles.testBox} ${styles.small}`}
-            >
-                <p>Date</p>
+            <div className={`${styles.Post__date} ${styles.small}`}>
+                <p>{FormatLocalDate(new Date())}</p>
             </div>
-            <div
-                className={`${styles.Post__author} ${styles.testBox} ${styles.small}`}
-            >
-                <p>Author</p>
+            <div className={`${styles.Post__author} ${styles.small}`}>
+                <p>{author}</p>
             </div>
-            <div
-                className={`${styles.Post__comments} ${styles.testBox} ${styles.small}`}
-            >
-                <p>Comments</p>
+            <div className={`${styles.Post__comments} ${styles.small}`}>
+                <span className="material-symbols-outlined">chat_bubble</span>
+                <p>{commentCount}</p>
             </div>
         </div>
     );
