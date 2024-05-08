ba0441eee179ca1f42377501b4e5a0f6e7b0da6e
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2024-05-08 10:37:30 -0600

    style: Update Comment and CommentList component styling

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
+import styles from "./Comment.module.scss";
+
 const Comment = () => {
     return (
-        <div>
-            <h3>Comment</h3>
-            <p>Comment text</p>
+        <div className={styles.Comment}>
+            <h4 className={styles.userName}>User Name</h4>
+            <p className={styles.text}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
         </div>
     );
 };
--- src/components/commentList/CommentList.jsx
+++ src/components/commentList/CommentList.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import Comment from "../comment/Comment";
 
+import styles from "./CommentList.module.scss";
+
 const CommentList = ({ parentClassName }) => {
     return (
-        <div className={parentClassName}>
-            <h3>Comments</h3>
+        <div className={`${parentClassName} ${styles.CommentList}`}>
+            <h3 className={`${styles.title}`}>Comments</h3>
+            <Comment />
+            <Comment />
+            <Comment />
             <Comment />
         </div>
     );
